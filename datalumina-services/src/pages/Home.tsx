836a76ce15f3c080f376ac4c8183f6794878d0cf
Haroon <haroonaziz295@gmail.com>
2025-02-28 23:18:49 +0500

    Home expertise

--- datalumina-services/src/pages/Home.tsx
+++ datalumina-services/src/pages/Home.tsx
@@ -2,12 +2,14 @@ import ClientsLogos from "../components/Homecomp/ClientsLogos";
 import HeroSection from "../components/Homecomp/HomeHero";
 import HomeServices from "../components/Homecomp/HomeServices";
 import HomeGIT from "../components/Homecomp/HomeGIT";
+import HExpertise from "../components/Homecomp/HExpertise";
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <ClientsLogos />
       <HomeServices />
+      <HExpertise />
       <HomeGIT />
     </div>
   );
